fa9349cdf6a8672177fd40a605db39d351e43b52
Marko Karajlovic <markokarajlovic@gmail.com>
2024-06-07 16:38:09 +0200

    added login button

--- lib/presentation/screens/login/login_screen.dart
+++ lib/presentation/screens/login/login_screen.dart
@@ -26,6 +26,25 @@ class LoginScreen extends StatelessWidget {
                 ),
               ),
             ),
+            const Spacer(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.yellow,
+                    foregroundColor: Colors.black,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12)
+                    )
+                  ),
+                  onPressed: () {},
+                  child: const Text('Uloguj se'),
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
           ],
         ),
       ),
