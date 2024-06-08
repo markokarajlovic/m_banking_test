8a0a1d19ce3a4ea9d595d6fb28d24340a0c25a3c
Marko Karajlovic <markokarajlovic@gmail.com>
2024-06-08 12:16:59 +0200

    added text description text

--- lib/presentation/screens/login/login_screen.dart
+++ lib/presentation/screens/login/login_screen.dart
@@ -8,6 +8,7 @@ class LoginScreen extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
               padding: const EdgeInsets.only(left: 16, top: 16),
@@ -26,19 +27,65 @@ class LoginScreen extends StatelessWidget {
                 ),
               ),
             ),
+            const Padding(
+              padding: EdgeInsets.only(left: 32),
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 32,
+                    height: 32,
+                    child: Placeholder(),
+                  ),
+                  SizedBox(width: 8),
+                  Text(
+                    'Raiffeisen\nBank',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      height: 0.9,
+                    ),
+                  ),
+                ],
+              ),
+            ),
             const Spacer(),
+            const Padding(
+              padding: EdgeInsets.only(left: 32, bottom: 8),
+              child: Text(
+                'DOBRO DOŠLI',
+                style: TextStyle(
+                  fontSize: 19,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 32, bottom: 16),
+              child: Text(
+                'Moja mBanka\nza stanovništvo',
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.w600,
+                  height: 0.9,
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 32, bottom: 16),
+              child: Text(
+                'Brzo, lako i sigurno',
+                style: TextStyle(color: Colors.black54),
+              ),
+            ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16),
               child: SizedBox(
                 width: double.infinity,
                 child: ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.yellow,
-                    foregroundColor: Colors.black,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12)
-                    )
-                  ),
+                      backgroundColor: Colors.yellow,
+                      foregroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12))),
                   onPressed: () {},
                   child: const Text('Uloguj se'),
                 ),
